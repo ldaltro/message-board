54eb26c37ef0781cf5473bb9e1a568a97c4cb8b7
Lucas Daltro <lucasdaltro5@gmail.com>
2016-01-08 19:29:02 -0500

    Add friendly urls to the messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :messages do
+  resources :messages, param: :title do
     resources :comments
   end
-  
+
   root 'messages#index'
 end
